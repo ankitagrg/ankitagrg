b984c6a19131ba54dffc1a4e68c27a08c117a75f
ankitagrg <ankitalg18@gmail.com>
2025-07-13 14:34:53 +0545

    Add route-based page components

--- src/pages/aboutme.jsx
+++ src/pages/aboutme.jsx
@@ -3,19 +3,21 @@ import Skills from "./skills";
 
 const AboutMe = () => {
   return (
-    <div id="about" className="flex flex-col items-center justify-center">
+    <div id="about" className="flex flex-col items-center justify-center py-14">
       <PageTitle title="ABOUT ME" />
-      <main className="max-w-6xl min-h-80 flex flex-col justify-center px-4 md:px-0">
-        <section className="mt-8 rounded-md p-4 shadow-inner transition-all duration-500 hover:scale-105">
+      <main className="max-w-6xl min-h-80 flex flex-col justify-center px-8 md:px-4 space-y-6">
+        <section className="bg-white shadow-md rounded-lg p-6 transition-transform duration-500 hover:scale-105">
           <h2 className="text-center text-2xl font-semibold text-gray-900">
-          Step into my creative space!
+            Step into my creative space!
           </h2>
-          <p className="mt-4 text-lg text-gray-700">
-            I'm currently pursuing my Software Engineering degree at Gandaki College of Engineering and Science, affiliated with Pokhara University.
-          </p>
-          <p className="mt-4 text-lg text-gray-700">
-          I am committed to building intelligent and innovative solutions that enhance digital experiences and drive technological evolution.
-          </p>
+          <div className="mt-4 space-y-4 text-lg text-gray-700">
+            <p>
+              I'm currently pursuing my Software Engineering degree at Gandaki College of Engineering and Science, affiliated with Pokhara University.
+            </p>
+            <p>
+              I am committed to building intelligent and innovative solutions that enhance digital experiences and drive technological evolution.
+            </p>
+          </div>
         </section>
         <Skills />
       </main>
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,19 +4,20 @@ import { projects } from "../utils/data";
 
 const Portfolio = () => {
   return (
-    <div id="projects" className="flex flex-col items-center justify-center p-8">
+    <div id="projects" className="flex flex-col items-center justify-center py-14 px-4">
       <PageTitle title="PROJECTS" />
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12 w-full max-w-6xl">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-10 w-full max-w-6xl">
         {projects.map((project) => (
           <ProjectCard
-            key={project.title}
-            date={project.date}
-            title={project.title}
-            description={project.description}
-            link={project.link}
-            technologies={project.technologies}
-            image={project.image}
-          />
+          key={project.title}
+          date={project.date}
+          title={project.title}
+          description={project.description}
+          link={project.link}
+          technologies={project.technologies}
+          images={project.images}  
+        />
+        
         ))}
       </div>
     </div>
