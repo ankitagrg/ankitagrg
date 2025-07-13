2085648bb9dda945ea5ed0bc3f9f97ca1659fffa
ankitagrg <ankitalg18@gmail.com>
2025-07-13 14:36:09 +0545

    Setup entry point and root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "../src/pages/home";
-import AboutMe from "./pages/AboutMe";
+import AboutMe from "./pages/aboutme";
 import Portfolio from "./pages/portfolio";
-
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-slate-50">
+    <div className="flex flex-col min-h-screen bg-[#f9fafb] text-black-900">
       <NavBar />
       <Home />
       <div className="px-4">
-        <AboutMe/>
+        <AboutMe />
         <Portfolio />
       </div>
       <Footer />
