0c8efc5f293b4f5b83a3259a6d053197382a0ebd
ankitagrg <ankitalg18@gmail.com>
2025-07-26 22:37:15 +0545

    Improve layout: add max-width and responsive container in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Home from "../src/pages/home";
+import Home from "./pages/home";
 import AboutMe from "./pages/aboutme";
 import Portfolio from "./pages/portfolio";
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-[#f9fafb] text-black-900">
+    <div className="flex flex-col min-h-screen bg-[#f9fafb] text-black">
       <NavBar />
       <Home />
-      <div className="px-4">
+      <div className="px-4 max-w-6xl mx-auto w-full">
         <AboutMe />
         <Portfolio />
       </div>
