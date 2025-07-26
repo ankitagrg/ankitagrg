fece5101e6177fce9af2bcd8b8b28901d63894b4
ankitagrg <ankitalg18@gmail.com>
2025-07-26 22:37:56 +0545

    Updated AboutMe content responsive

--- src/pages/aboutme.jsx
+++ src/pages/aboutme.jsx
@@ -5,12 +5,12 @@ const AboutMe = () => {
   return (
     <div id="about" className="flex flex-col items-center justify-center py-14">
       <PageTitle title="ABOUT ME" />
-      <main className="max-w-6xl min-h-80 flex flex-col justify-center px-8 md:px-4 space-y-6">
+      <main className="max-w-6xl min-h-80 flex flex-col justify-center px-4 space-y-6">
         <section className="bg-white shadow-md rounded-lg p-6 transition-transform duration-500 hover:scale-105">
-          <h2 className="text-center text-2xl font-semibold text-gray-900">
+          <h2 className="text-center text-xl md:text-2xl font-semibold text-gray-900">
             Step into my creative space!
           </h2>
-          <div className="mt-4 space-y-4 text-lg text-gray-700">
+          <div className="mt-4 space-y-4 text-base md:text-lg text-gray-700">
             <p>
               I'm currently pursuing my Software Engineering degree at Gandaki College of Engineering and Science, affiliated with Pokhara University.
             </p>
