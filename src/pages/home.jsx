601249039ce90dfb0d754e340bb6b5b6144ec499
ankitagrg <ankitalg18@gmail.com>
2025-07-26 22:37:19 +0545

    Refactor Home page spacing and text size for better responsiveness

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,21 +3,15 @@ import SocialMedia from "../components/SocialMedia";
 
 const Home = () => {
   return (
-    <div
-      id="home"
-      className="flex flex-col mt-24 max-w-6xl w-11/12 mx-auto"
-    >
+    <div id="home" className="flex flex-col mt-12 md:mt-24 max-w-6xl w-11/12 mx-auto">
       <div>
         <Name text="ANKITA" />
       </div>
-
-      <section >
-        <p className="font-marcellus text-black900 text-lg tracking-wide leading-relaxed">
-        is a driven and creative individual with a passion for technology and innovation. 
+      <section>
+        <p className="font-marcellus text-black text-base md:text-lg tracking-wide leading-relaxed">
+          is a driven and creative individual with a passion for technology and innovation.
         </p>
       </section>
-
-      {/* Left side content */}
       <section className="flex flex-col items-start">
         <SocialMedia />
       </section>
