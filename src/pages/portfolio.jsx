049c1d43bd87d82a0b88992635f60f956569584f
ankitagrg <ankitalg18@gmail.com>
2025-07-26 22:38:31 +0545

    Update Portfolio grid

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -9,15 +9,14 @@ const Portfolio = () => {
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-10 w-full max-w-6xl">
         {projects.map((project) => (
           <ProjectCard
-          key={project.title}
-          date={project.date}
-          title={project.title}
-          description={project.description}
-          link={project.link}
-          technologies={project.technologies}
-          images={project.images}  
-        />
-        
+            key={project.title}
+            date={project.date}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+            technologies={project.technologies}
+            images={project.images}
+          />
         ))}
       </div>
     </div>
