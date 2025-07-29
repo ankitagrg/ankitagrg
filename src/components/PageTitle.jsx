40a96051c998eb02241dac6d8d35995b170370a8
ankitagrg <ankitalg18@gmail.com>
2025-07-29 18:16:11 +0545

    Updated Title

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -2,15 +2,19 @@ import { motion } from "framer-motion";
 
 const PageTitle = ({ title }) => {
   const titleVariants = {
-    initial: { opacity: 0, x: -100 },  // Starts from the left side
-    animate: { opacity: 1, x: 0, transition: { duration: 1.5} },  
+    initial: { opacity: 0, x: -50 },
+    animate: { opacity: 1, x: 0, transition: { duration: 1 } },
   };
 
   return (
     <div className="flex flex-col items-center justify-center my-8">
-      {/* Hidden part for the initial fade-in effect */}
+      <h1 className="text-4xl md:text-5xl font-marcellus font-semibold opacity-10 mt-8">
+        {title}
+      </h1>
+
+      {/* Animated main title */}
       <motion.h1
-        className="text-3xl font-semibold bg-clip-text bg-gradient-to-r from-blue-500"
+        className="md:text-3xl text-2xl font-marcellus text-black-800 -mt-8"
         variants={titleVariants}
         initial="initial"
         whileInView="animate"
