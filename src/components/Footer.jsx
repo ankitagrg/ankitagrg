7deb32758c38913cb03d3feb5f3b0ad0a9671a0a
ankitagrg <ankitalg18@gmail.com>
2025-07-31 12:37:57 +0545

    Update on footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="p-2 flex justify-between items-center w-full text-black">
-      {/* Left side content */}
       <div className="flex-1 text-center">
         <p className="font-normal font-pixel text-sm sm:text-base lg:text-lg text-tertiary">
         &copy; {new Date().getFullYear()} Ankita. All Rights Reserved
