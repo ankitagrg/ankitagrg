162e4b73269d52a02a4a00cabedc1edf4539feee
ankitagrg <ankitalg18@gmail.com>
2025-07-31 10:40:46 +0545

    Slightly update on Project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,16 @@ import PropTypes from "prop-types";
 import { motion } from "framer-motion";
 import ProjectModal from "./ProjectModal";
 
-const ProjectCard = ({ title, description, link, date, technologies, images, demo, impact }) => {
+const ProjectCard = ({
+  title,
+  description,
+  link,
+  date,
+  images,
+  demo,
+  impact,
+  technologies, 
+}) => {
   const [currentImage, setCurrentImage] = useState(0);
   const [intervalId, setIntervalId] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
@@ -37,41 +46,38 @@ const ProjectCard = ({ title, description, link, date, technologies, images, dem
   return (
     <>
       <motion.div
-        className="flex flex-col h-full gap-4 py-4 shadow-md rounded-lg hover:shadow-lg transition-shadow"
-        whileHover={{ scale: 1.05 }}
-        transition={{ duration: 0.3 }}
+        className="flex flex-col h-full py-4 shadow-md rounded-xl bg-white hover:shadow-lg transition-shadow duration-300"
+        whileHover={{ scale: 1.03 }}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
+        {/* Project Image */}
         <motion.img
           src={images[currentImage]}
           alt={title}
-          className="w-full object-contain"
-          initial={{ x: '100%' }}
-          animate={{ x: '0%' }}
+          className="w-full h-48 object-contain rounded-t-xl"
+          initial={{ x: "100%" }}
+          animate={{ x: "0%" }}
           transition={{
-            type: 'spring',
+            type: "spring",
             stiffness: 150,
             damping: 20,
             duration: 0.5,
           }}
         />
-
-        <div className="px-4">
-          <h3 className="text-[22px] font-bold text-black-900 hover:text-blue-800 hover:underline">
+        <div className="flex flex-col flex-grow px-4 mt-3 space-y-2">
+          <h3 className="text-lg md:text-xl font-semibold font-marcellus text-pri hover:text-blue-800 hover:underline">
             {title}
           </h3>
-          <p className="mt-1 text-sm text-black-900">{date}</p>
+          <p className="text-sm text-black">{date}</p>
+          <p className="text-sm  text-black flex-grow line-clamp-3 md:line-clamp-none">
+            {description}
+          </p>
         </div>
-
-        <p className="text-black-900 px-4 flex-grow line-clamp-3 md:line-clamp-none">
-          {description}
-        </p>
-
-        <div className="px-4 pb-4">
+        <div className="px-4 pt-4 mt-auto">
           <button
             onClick={() => setIsOpen(true)}
-            className="inline-flex items-center text-black-900 hover:text-blue-900 transition-all duration-200 group"
+            className="inline-flex items-center text-black hover:text-blue-900 transition-all duration-200 group"
           >
             View Project
             <span className="ml-1 transform group-hover:translate-x-1 transition-transform duration-200">
@@ -84,7 +90,16 @@ const ProjectCard = ({ title, description, link, date, technologies, images, dem
       <ProjectModal
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
-        project={{ title, description, date, technologies, images, link, demo, impact }}
+        project={{
+          title,
+          description,
+          date,
+          images,
+          link,
+          demo,
+          impact,
+          technologies, 
+        }}
       />
     </>
   );
@@ -95,9 +110,9 @@ ProjectCard.propTypes = {
   description: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
-  technologies: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
   demo: PropTypes.string,
+  technologies: PropTypes.string.isRequired,
   impact: PropTypes.string,
 };
 
