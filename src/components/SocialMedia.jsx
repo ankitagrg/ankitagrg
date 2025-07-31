5ce60b11a94fa842ce41eb07a4f8a1eb842244dc
ankitagrg <ankitalg18@gmail.com>
2025-07-31 10:43:26 +0545

    Update on Socialmedia

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,10 +6,9 @@ export const GITHUB_URL = "https://github.com/ankitagrg";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/ankita-gurung-97312a2a8/";
 export const MAIL = "ankitagurung43@gmail.com";
 
-export const SocialMedia = () => {
+const SocialMedia = () => {
   return (
     <div className="flex justify-start items-center gap-2 mt-1">
-     
       <IconContext.Provider
         value={{
           size: "24px",
@@ -17,13 +16,28 @@ export const SocialMedia = () => {
             "text-black hover:text-black-800 transition-transform duration-300 transform hover:scale-110",
         }}
       >
-        <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+        <a
+          href={GITHUB_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
           <BsGithub />
         </a>
-        <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+        <a
+          href={LINKEDIN_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
           <BsLinkedin />
         </a>
-        <a href={`mailto:${MAIL}`} target="_blank" rel="noopener noreferrer" aria-label="Email">
+        <a
+          href={`mailto:${MAIL}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Email"
+        >
           <LuMail />
         </a>
       </IconContext.Provider>
