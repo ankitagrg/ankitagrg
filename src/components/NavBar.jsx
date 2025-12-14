1ba533ff079c4812d78b34a90a12a78a6c6625c4
ankitagrg <ankitalg18@gmail.com>
2025-12-14 22:30:23 +0545

    Slightly change on about section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,8 +27,8 @@ const NavBar = () => {
           ))}
         </div>
         <a
-          href="/ANKITAGURUNG_CV.pdf"
-          download="ANKITAGURUNG_CV.pdf"
+          href="/ANKITAGURUNG_CV"
+          download="ANKITAGURUNG_CV"
           className="inline-flex items-center bg-blue-900 hover:bg-blue-700 text-white text-sm font-medium px-4 py-2 rounded-md shadow-sm"
         >
           📥 CV
